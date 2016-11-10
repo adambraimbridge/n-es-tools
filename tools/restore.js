f14d8575bdca6822f85008e1342742bef03d632a
Matt Hinchliffe <matt.hinchliffe@ft.com>
2016-11-10 18:27:25 +0000

    Add snapshot restore tool

---
+++ tools/restore.js
@@ -0,0 +1,74 @@
+const progress = require('../lib/progress')
+const elastic = require('../lib/elastic')
+const wait = require('../lib/wait')
+
+let client
+let status
+
+function verifyRepository ({ repository }) {
+  return client.snapshot.verifyRepository({ repository })
+}
+
+function restoreSnapshot ({ repository, snapshot, index }) {
+  return client.snapshot.restore({
+    repository,
+    snapshot,
+    waitForCompletion: false,
+    body: {
+      indices: index,
+      // we're restoring an index, not the cluster
+      include_aliases: false
+    }
+  })
+    .then((response) => {
+      if (!response.accepted) {
+        throw new Error('Nothing to restore')
+      }
+    })
+}
+
+function pingStatus ({ snapshot, index }) {
+  return client.indices.recovery({
+    index,
+    activeOnly: true
+  })
+    .then((response) => {
+      // we'll just take the status of the primary shard for simplicity
+      const stats = response[index].shards.find((item) => item.source.snapshot === snapshot)
+
+      if (stats) {
+        status.total = stats.index.files.total
+        status.curr = stats.index.files.recovered
+
+        status.tick()
+
+        if (stats.stage === 'DONE') {
+          return
+        }
+      }
+
+      return wait(10000).then(() => pingStatus({ snapshot, index }))
+    })
+}
+
+function run (region, opts) {
+  client = elastic(region)
+  status = progress('Restoring snapshot')
+
+  return Promise.resolve()
+    .then(() => verifyRepository(opts))
+    .then(() => restoreSnapshot(opts))
+    .then(() => pingStatus(opts))
+    .then(() => console.log(`Restored snapshot "${opts.snapshot}" to ${region} region`))
+    .catch((err) => console.error(`Restore failed: ${err.message}`))
+}
+
+module.exports = function (program) {
+  program
+    .command('restore <region>')
+    .description('Restores an index snapshot')
+    .option('-I, --index <name>', 'The index name', 'v3_api_v2')
+    .option('-S, --snapshot <name>', 'The snapshot name', 'my-snapshot')
+    .option('-R, --repository <name>', 'The repository name', 's3-snapshots')
+    .action(run)
+}
