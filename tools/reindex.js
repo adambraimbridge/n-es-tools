d3e7b1a23a6424773a437a9c7350302f414d7335
Matt Hinchliffe <matt.hinchliffe@ft.com>
2017-12-05 13:02:30 +0000

    Test reindex tool on local cluster and buff rough edges  🐿 v2.5.16

--- tools/reindex.js
+++ tools/reindex.js
@@ -6,47 +6,69 @@ const beep = require('beepbeep')
 let client
 let status
 
-function verifyIndices ({ source, destination }) {
+function verifyIndices ({ source, dest }) {
   return client.cat.indices({
     format: 'json'
   })
     .then((result) => {
       const a = result.some(({ index }) => index === source)
-      const b = result.some(({ index }) => index === destination)
+      const b = result.some(({ index }) => index === dest)
 
       if (a === false) {
-        return Promise.reject(`Could not find the index "${source}"`)
+        return Promise.reject(`Could not find the index ${source}"`)
       }
 
       if (b === false) {
-        return Promise.reject(`Could not find the index "${destination}"`)
+        return Promise.reject(`Could not find the index "${dest}"`)
       }
     })
 }
 
-function startReindex ({ source, destination }) {
-  return client.reindex({ source, destination, waitForCompletion: false })
+function startReindex ({ source, dest }) {
+  return client.reindex({
+    body: {
+      source: {
+        index: source
+      },
+      dest: {
+        index: dest
+      }
+    },
+    waitForCompletion: false
+  })
 }
 
 function pingStatus (taskId) {
   return client.tasks.get({ taskId })
     .then((result) => {
-      if (result.completed) {
-        status.total = result.task.status.total
-        status.curr = result.task.status.created
+      status.total = result.task.status.total
+      status.curr = result.task.status.created
 
+      // Don't draw a progress bar before we have any data
+      // and don't draw one when AWS gets carried away.
+      if (status.total > 0 && status.curr <= status.total) {
         status.tick()
+      }
 
-        return result.response
-      } else if (result.errors) {
+      if (result.completed) {
+        if (result.failures.length) {
+          return Promise.reject(
+            new Error(`Task completed but with ${result.failures.length} failures`)
+          )
+        } else {
+          return result.response
+        }
+      }
+
+      if (result.errors) {
         status.terminate()
 
         return Promise.reject(
           new Error(result.errors.join(', '))
         )
-      } else {
-        return wait(10000).then(() => pingStatus(taskId))
       }
+
+      return wait(10000).then(() => pingStatus(taskId))
     })
 }
 
@@ -59,28 +81,26 @@ function run (cluster, command) {
   return Promise.resolve()
     .then(() => verifyIndices(opts))
     .then(() => startReindex(opts))
-    .then(({ taskId }) => {
-      console.log(`Reindex started with task ID ${taskId}`)
-      return pingStatus(taskId)
+    .then(({ task }) => {
+      console.log(`Reindex started with task ID ${task}`)
+      return pingStatus(task)
     })
     .then(() => {
-      console.log(`Reindex from ${opts.source} to ${opts.destination} complete`)
+      console.log(`Reindex from ${opts.source} to ${opts.dest} complete`)
+      beep(2)
       process.exit()
     })
     .catch((err) => {
       console.error(`Reindex failed: ${err.toString()}`)
       process.exit(1)
     })
-    .then(() => {
-      beep(2)
-    })
 }
 
 module.exports = function (program) {
   program
-    .command('reindex <cluster> <dest>')
-    .description('Restores an index snapshot')
+    .command('reindex <cluster>')
+    .description('Copies content from one index to another')
     .option('-S, --source <name>', 'The source index name', 'content')
-    .option('-D, --destination <name>', 'The destination index name', 'content_{{date}}')
+    .option('-D, --dest <name>', 'The destination index name')
     .action(run)
 }
